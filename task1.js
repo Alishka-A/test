a1e35d454b27746fa793f9c95cc290badf399750
Alishka <alishkaalpha01@gmail.com>
2024-09-23 16:08:35 +0400

    homework1

---
+++ task1.js
@@ -0,0 +1,58 @@
+let a = "hello world"
+console.log(typeof(a))
+let b = 5
+console.log(typeof(b))
+let c = true
+console.log(typeof(c))
+let d
+console.log(typeof(d))
+let e = null
+console.log(typeof(e))
+let f = Symbol(10)
+console.log(typeof(f))
+let g = BigInt(2 ^ 999)
+console.log(typeof(g))
+
+
+
+let apple = 10 // let is the new way of declaring variables starting ES6(ES2015). | let is bloked scoped 
+
+const apple_pie = 50 // the const keyword was also introduced in the ES6(2015) version to create constants | 
+    // Once a constant is initialized, we cannot change its value 
+
+var pineapple = 20 // var is used in the older versions of javascript | var is function scoped 
+
+
+console.log({ Firstname: `Jason`, Lastname: `Anderson`, age: 50, country: `USA`, hobby: `playing piano`, occupation: `None` })
+
+
+
+
+
+
+
+let address = 'Yasamal'
+let num = 11
+
+console.log(`My address is ${address} street, building number is ${num}.`)
+
+
+console.log(5 + 7, 5 - 2, 5 * 5, 15 / 3, 20 % 3)
+
+console.log(2 == '2', 2 == 5) // true , false 
+console.log(2 === 2, 2 === '2') // true , false
+console.log(5 > 2, 5 > 5) // true ,false 
+console.log(4 < 6, 5 < 4) //true , false
+console.log(3 != 2) // true
+console.log(2 !== 2, 2 !== '2') //false,true
+console.log(5 >= 5, 5 >= 4) // true,true
+console.log(4 <= 3, 3 <= 3) //false true
+
+
+
+console.log(true && true) //true
+console.log(true && false) //false
+
+console.log(true || false) //true
+
+console.log(!true) //false
